c247fe2f7ba46dd556d0fc972cb10a103a13f88a
ShakiraOrosco <shakiraog2005@gmail.com>
2025-09-29 17:27:22 -0400

    reseteo de la estructura por division de backend y frontend

--- Backend/apps/reservas/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ReservaGen
-
-class ReservaGenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ReservaGen
-        fields = "__all__"
