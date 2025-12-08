be65ccd1a1922212610472a17c9701f35cc3aa94
ShakiraOrosco <shakiraog2005@gmail.com>
2025-12-08 18:35:16 -0400

    configuracion de permisos

--- apps/auditoria/views.py
+++ apps/auditoria/views.py
@@ -1,12 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Auditoria
 from .serializers import AuditoriaSerializer
 
 
+
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([AllowAny])
 def listar_auditorias(request):
     """
     Endpoint para listar auditorías.
