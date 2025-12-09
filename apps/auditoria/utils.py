cfc39672b5659282eff0782e825fd7eeb6a5609b
ShakiraOrosco <shakiraog2005@gmail.com>
2025-12-08 21:34:46 -0400

    Auditorias para la tabla de usaurios tanto creacion

--- apps/auditoria/utils.py
+++ apps/auditoria/utils.py
@@ -18,3 +18,57 @@ def registrar_login(request, user, usuario=None):
         tabla='auth_user',
         descripcion=descripcion
     )
+
+
+
+
+###################################################################
+################## AUDITORIA DE USUARIOS ##########################
+###################################################################
+
+def registrar_creacion_usuario(request, user, usuario):
+    """Crea un registro de auditoría para la creación de un usuario."""
+    descripcion = f"El usuario {user.username} registró el usuario {usuario.nombre} {usuario.app_paterno or ''} {usuario.app_materno or ''}."
+    
+    Auditoria.objects.create(
+        usuario=user,
+        accion='REGISTRO',
+        tabla='usuario',
+        descripcion=descripcion
+    )
+
+def registrar_actualizacion_usuario(request, user, usuario, datos_viejos, datos_nuevos):
+    """Crea un registro de auditoría para la actualización de un usuario, campo por campo."""
+    # Iterar sobre los datos nuevos para comparar con los viejos
+    for campo, valor_nuevo in datos_nuevos.items():
+        # Obtener el valor viejo para comparación
+        valor_viejo = datos_viejos.get(campo)
+
+        # Si el valor ha cambiado, registrar la auditoría
+        if valor_viejo != valor_nuevo:
+            descripcion = (
+                f"El usuario {user.username} actualizó el {campo} del usuario "
+                f"{usuario.nombre} {usuario.app_paterno or ''} {usuario.app_materno or ''} "
+                f"de '{valor_viejo}' a '{valor_nuevo}'."
+            )
+
+            Auditoria.objects.create(
+                usuario=user,
+                accion='ACTUALIZACIÓN',
+                tabla='usuario',
+                descripcion=descripcion
+            )
+
+
+def registrar_estado_usuario(request, user, usuario):
+    """Crea un registro de auditoría para el cambio de estado de un usuario."""
+    estado = "habilitó" if usuario.activo else "deshabilitó"
+    descripcion = f"El usuario {user.username} {estado} al usuario {usuario.nombre} {usuario.apellido_paterno or ''} {usuario.apellido_materno or ''}."
+    
+    Auditoria.objects.create(
+        usuario=user,
+        accion='ACTUALIZACIÓN',
+        tabla='usuarios',
+        descripcion=descripcion
+    )
+
